52c59ca6b29f7a04d63f42c43f61b35c39c1b50d
Anton <anton1.a@mail.ru>
2018-12-16 19:48:30 +0300

    Add auth components

---
+++ pages/auth.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="auth">
+    <div class="auth-form form">
+      <component :is="currentComponent"></component>
+    </div>
+
+    <div>
+      <button @click="switchComponent">Переключить</button>
+    </div>
+    
+  </section>
+</template>
+
+<script>
+import Login from '~/components/Login'
+import Signup from '~/components/Signup'
+
+export default {
+  components: {
+    Login,
+    Signup
+  },
+  data() {
+    return {
+      currentComponent: "login"
+    }
+  },
+  methods: {
+    switchComponent() {
+      this.currentComponent = (this.currentComponent === 'login') ? 'signup' : 'login' 
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import '~assets/scss/components/auth';
+</style>
