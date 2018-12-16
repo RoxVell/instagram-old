a660dc2969039b6ab804593dc8f5642a94100270
Anton <anton1.a@mail.ru>
2018-12-16 19:48:30 +0300

    Add auth components

---
+++ components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <p class="form-title">Авторизуйтесь</p>
+
+    <div class="form-element">
+      <label for="auth-login">Логин</label>
+      <input id="auth-login" type="text">
+    </div>
+    
+    <div class="form-element">
+      <label for="login-password" >Пароль</label>
+      <input id="login-password" type="password">
+    </div>
+
+    <div class="form-element">
+      <button class="btn btn-primary">Войти</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ components/Signup.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="signup">
+    <p class="form-title">Зарегистрируйтесь</p>
+    <div class="form-element">
+      <label class="required" for="signup-mail">E-mail</label>
+      <input id="signup-mail" type="email">
+    </div>
+    
+    <div class="form-element">
+      <label class="required" for="signup-password">Пароль</label>
+      <input id="signup-password" type="text">
+    </div>
+
+    <div class="form-element">
+      <label class="required" for="signup-password-repeat">Подтвердите пароль</label>
+      <input id="signup-password-repeat" type="text">
+    </div>
+   
+    <div class="form-element">
+      <button class="btn btn-primary">Регистрация</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
