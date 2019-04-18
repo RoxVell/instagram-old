4130777e9f49a18d807ae2d4e02aff14e6bfcce2
Anton <anton1.a@mail.ru>
2019-04-18 20:49:52 +0300

    FileUploader component: the ability to drag items

--- components/FileUploader.vue
+++ components/FileUploader.vue
@@ -8,12 +8,12 @@
           <span class="format-desc">PNG, JPG, GIF up to 10MB</span>
         </div>
       </div>
-      
+
       <div class="format">
         <span class="format-icon format-icon__video"></span>
         <div class="format-info">
           <span class="format-title">Video</span>
-         <span class="format-desc">MP4 or WEBM, 4:3, 24 sec</span>
+          <span class="format-desc">MP4 or WEBM, 4:3, 24 sec</span>
         </div>
       </div>
     </div>
@@ -22,25 +22,27 @@
       <div id="drop-mask"></div>
       <div key="1" v-if="images && images.length !== 0" class="upload-section__images">
         <div class="images" v-if="images.length !== 0">
-          
-          <div
-            class="images-item"
-            v-for="(image, index) in images"
-            :key="index"
-            draggable="true"
-            @dragstart="handleDragStart($event)"
-            @drop="handleDragDrop($event)"
-            @dragend="handleDragEnd($event)"
-            @dragleave="handleDragLeave($event)"
-          >
-            <div class="images-drop-mask" @dragenter="handleDragEnter($event)"></div>
-            <div class="images-item__picture" :style="{ backgroundImage: `url(${image.src})`}"></div>
-            <p class="images-item__name">{{ image.name }}</p> 
-            <div class="images-item__actions">
-              <button class="btn-link">Редактировать</button>
-              <button class="btn-link" @click="deleteFile(index)">Удалить</button>
+          <transition-group name="flip-list">
+            <div
+              class="images-item"
+              v-for="(image, index) in images"
+              :key="image.name"
+              :itemOrder="index"
+              draggable="true"
+              @dragstart="handleDragStart($event)"
+              @drop="handleDragDrop($event)"
+              @dragend="handleDragEnd($event)"
+              @dragleave="handleDragLeave($event)"
+            >
+              <div class="images-drop-mask" @dragenter="handleDragEnter($event)"></div>
+              <div class="images-item__picture" :style="{ backgroundImage: `url(${image.src})`}"></div>
+              <p class="images-item__name">{{ image.name }}</p>
+              <div class="images-item__actions">
+                <button class="btn-link">Редактировать</button>
+                <button class="btn-link" @click="deleteFile(index)">Удалить</button>
+              </div>
             </div>
-          </div>
+          </transition-group>
         </div>
       </div>
 
@@ -55,27 +57,46 @@
             @change="selectFiles($event)"
           >
           <label for="file">
-            <svg class="icon-upload" version="1.1" id="upload-icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 486.3 486.3" style="enable-background:new 0 0 486.3 486.3;" xml:space="preserve">
+            <svg
+              class="icon-upload"
+              version="1.1"
+              id="upload-icon"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 486.3 486.3"
+              style="enable-background:new 0 0 486.3 486.3;"
+              xml:space="preserve"
+            >
               <g>
-                <path d="M395.5,135.8c-5.2-30.9-20.5-59.1-43.9-80.5c-26-23.8-59.8-36.9-95-36.9c-27.2,0-53.7,7.8-76.4,22.5
+                <path
+                  d="M395.5,135.8c-5.2-30.9-20.5-59.1-43.9-80.5c-26-23.8-59.8-36.9-95-36.9c-27.2,0-53.7,7.8-76.4,22.5
                   c-18.9,12.2-34.6,28.7-45.7,48.1c-4.8-0.9-9.8-1.4-14.8-1.4c-42.5,0-77.1,34.6-77.1,77.1c0,5.5,0.6,10.8,1.6,16
                   C16.7,200.7,0,232.9,0,267.2c0,27.7,10.3,54.6,29.1,75.9c19.3,21.8,44.8,34.7,72,36.2c0.3,0,0.5,0,0.8,0h86
                   c7.5,0,13.5-6,13.5-13.5s-6-13.5-13.5-13.5h-85.6C61.4,349.8,27,310.9,27,267.1c0-28.3,15.2-54.7,39.7-69
                   c5.7-3.3,8.1-10.2,5.9-16.4c-2-5.4-3-11.1-3-17.2c0-27.6,22.5-50.1,50.1-50.1c5.9,0,11.7,1,17.1,3c6.6,2.4,13.9-0.6,16.9-6.9
                   c18.7-39.7,59.1-65.3,103-65.3c59,0,107.7,44.2,113.3,102.8c0.6,6.1,5.2,11,11.2,12c44.5,7.6,78.1,48.7,78.1,95.6
                   c0,49.7-39.1,92.9-87.3,96.6h-73.7c-7.5,0-13.5,6-13.5,13.5s6,13.5,13.5,13.5h74.2c0.3,0,0.6,0,1,0c30.5-2.2,59-16.2,80.2-39.6
-                  c21.1-23.2,32.6-53,32.6-84C486.2,199.5,447.9,149.6,395.5,135.8z"/>
-                <path class="icon-upload__arrow" d="M324.2,280c5.3-5.3,5.3-13.8,0-19.1l-71.5-71.5c-2.5-2.5-6-4-9.5-4s-7,1.4-9.5,4l-71.5,71.5c-5.3,5.3-5.3,13.8,0,19.1
+                  c21.1-23.2,32.6-53,32.6-84C486.2,199.5,447.9,149.6,395.5,135.8z"
+                ></path>
+                <path
+                  class="icon-upload__arrow"
+                  d="M324.2,280c5.3-5.3,5.3-13.8,0-19.1l-71.5-71.5c-2.5-2.5-6-4-9.5-4s-7,1.4-9.5,4l-71.5,71.5c-5.3,5.3-5.3,13.8,0,19.1
                   c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4l48.5-48.5v222.9c0,7.5,6,13.5,13.5,13.5s13.5-6,13.5-13.5V231.5l48.5,48.5
-                  C310.4,285.3,318.9,285.3,324.2,280z"/>
+                  C310.4,285.3,318.9,285.3,324.2,280z"
+                ></path>
               </g>
             </svg>
           </label>
         </div>
-      
+
         <div class="upload-section__description">
           <p>Drag and drop to upload</p>
-          <p>or <label for="file">browse</label> to choose a file</p>
+          <p>
+            or
+            <label for="file">browse</label> to choose a file
+          </p>
         </div>
       </div>
     </div>
@@ -91,7 +112,7 @@ export default {
       default() {
         return ['']
       }
-    },
+    }
   },
   data() {
     return {
@@ -101,7 +122,7 @@ export default {
   },
   computed: {
     images() {
-      return this.currentFiles.map(file => {
+      return this.currentFiles.map((file) => {
         return {
           name: file.name,
           src: window.URL.createObjectURL(file)
@@ -111,39 +132,81 @@ export default {
   },
   methods: {
     handleDragEnd(event) {
-      const dropItemMasks = Array.from(document.querySelectorAll('.images-drop-mask'))
+      event.target.classList.remove('drag')
 
-      event.target.style.opacity = '1'
-      dropItemMasks.forEach(item => item.style.display = 'none')
+      const dropItemMasks = Array.from(
+        document.querySelectorAll('.images-drop-mask')
+      )
+      dropItemMasks.forEach((item) => (item.style.display = 'none'))
 
       this.currentItem = null
     },
     handleDragDrop(event) {
-      event.target.parentNode.style.borderTopColor = ''
+      const changeItem = event.target.parentNode
+
+      let selectedItemOrder = changeItem.attributes['itemorder'].value
+      let changeItemOrder = this.currentItem.attributes['itemorder'].value
+
+      if (!changeItem.isSameNode(this.currentItem)) {
+        this.currentFiles = this.swapElement(
+          this.currentFiles,
+          selectedItemOrder,
+          changeItemOrder
+        )
+        console.log(this.currentFiles)
+      }
+
+      changeItem.style.borderTopColor = ''
       this.currentItem = null
     },
     handleDragStart(event) {
       this.currentItem = event.target
-      const dropItemMasks = Array.from(document.querySelectorAll('.images-drop-mask'))
 
-      dropItemMasks.forEach(item => item.style.display = 'block')
+      document
+        .querySelectorAll('.images-drop-mask')
+        .forEach((item) => (item.style.display = 'block'))
 
       event.dataTransfer.setData('Text', event.target.id)
-      event.target.style.opacity = '0.4'
       event.dataTransfer.dropEffect = 'move'
+      event.target.classList.add('drag')
     },
     handleDragEnter(event) {
-      document.querySelector('.images')
-        .insertBefore(this.currentItem, event.target.parentElement)
+      const currentTargetItem = event.target.parentNode
+      currentTargetItem.style.borderTopColor = '#0366D6'
+      const items = document.querySelectorAll('div.images-item')
+      // let selectedItem = null
+      // let changeItem = null
+
+      // for (let i = 0; i < items.length; i++) {
+      //   if (items[i].isSameNode(event.target.parentNode)) selectedItem = i
+      //   else if (items[i].isSameNode(this.currentItem)) changeItem = i
+      // }
+
+      // if (!items[selectedItem].isSameNode(this.currentItem)) {
+      //   this.currentFiles = this.swapElement(
+      //     this.currentFiles,
+      //     selectedItem,
+      //     changeItem
+      //   )
+      // }
+
+      // this.currentFiles = this.swapElement(this.currentFiles, 0, 1)
+      // document
+      //   .querySelector('.images span')
+      //   .insertBefore(this.currentItem, event.target.parentElement)
     },
     handleDragLeave(event) {
       event.target.parentNode.style.borderTopColor = ''
     },
     selectFiles(event) {
-      const files = Array.from((event.type === 'change') ? event.target.files : event.dataTransfer.files)
+      const files = Array.from(
+        event.type === 'change' ? event.target.files : event.dataTransfer.files
+      )
 
-      const isFilesFormats = files.every(file => {
-        return this.acceptFileFormats.some(format => file.type.startsWith(format))
+      const isFilesFormats = files.every((file) => {
+        return this.acceptFileFormats.some((format) =>
+          file.type.startsWith(format)
+        )
       })
 
       if (isFilesFormats) {
@@ -151,6 +214,13 @@ export default {
       } else {
         // TODO: wrong formats
       }
+    },
+    swapElement(arr, oldIndex, newIndex) {
+      const copyArray = [...arr]
+      const temp = copyArray[oldIndex]
+      copyArray[oldIndex] = copyArray[newIndex]
+      copyArray[newIndex] = temp
+      return copyArray
     },
     deleteFile(index) {
       this.currentFiles.splice(index, 1)
@@ -185,15 +255,15 @@ export default {
     removeListeners() {
       const uploadSection = document.querySelector('.upload-section')
       const dropMask = document.querySelector('#drop-mask')
-      uploadSection.ondragover = null
       uploadSection.ondragenter = null
+      uploadSection.ondragover = null
       uploadSection.ondrop = null
       dropMask.ondragleave = null
       dropMask.ondrop = null
     },
     containsFiles(event) {
       const { types } = event.dataTransfer
-      return types && types.some(type => type === 'Files')
+      return types && types.some((type) => type === 'Files')
     }
   },
   beforeDestroy() {
