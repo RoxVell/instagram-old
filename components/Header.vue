12fdb273d80b90da294c10340ca990be6b45b2f4
RoxVell <anton1.a@mail.ru>
2019-07-26 21:33:25 +0300

    Header styling

--- components/Header.vue
+++ components/Header.vue
@@ -8,26 +8,43 @@
       <Search />
 
       <div class="header-auth">
+        <nav>
+          <ul class="header-menu">
+            <template v-if="$store.getters['user/isAuth']">
+              <nuxt-link to="/upload" class="btn" tag="li">
+                <img
+                  style="width: 18px; margin-right: 7px;"
+                  src="/images/icons/upload-min.png"
+                  alt="upload icon"
+                />
+                <span>Добавить фото</span>
+              </nuxt-link>
 
-        <template v-if="$store.getters['user/isAuth']">
-          <nuxt-link to="/upload" class="btn" tag="a">Добавить фото</nuxt-link>
-          <nuxt-link class="user-section" :to='`/user/${user.username}`' tag="a">
-            <img class="user-avatar" :src="user.profile_picture" alt="User avatar">
-            <span class="user-name">{{ user.username }}</span>
-          </nuxt-link>
-        </template>
+              <nuxt-link :to="`/user/${user.username}`" tag="li">
+                <img
+                  style="width: 30px; height: 30px; margin-right: 10px;"
+                  class="user-avatar"
+                  :src="user.profile_picture"
+                  alt="avatar"
+                />
+                <!-- <img
+                  style="width: 18px; margin-right: 7px;"
+                  src="/images/icons/user-outline.svg"
+                  alt
+                />-->
+                <span class="user-name">{{ user.username }}</span>
+              </nuxt-link>
 
-        <template v-else>
-          <ul class="auth">
-            <nuxt-link to="/auth/login" tag="a">
-              <li class="auth-login">Войти</li>
-            </nuxt-link>
-            <nuxt-link to="/auth/signup" tag="a">
-              <li class="auth-signup">Зарегистрироваться</li>
-            </nuxt-link>
+              <li class="header-menu__break"></li>
+              <li @click="signOut">Выйти</li>
+            </template>
+
+            <template v-else>
+              <nuxt-link to="/auth/login" tag="li">Войти</nuxt-link>
+              <nuxt-link to="/auth/signup" tag="li">Зарегистрироваться</nuxt-link>
+            </template>
           </ul>
-        </template>
-
+        </nav>
       </div>
     </div>
   </header>
@@ -44,16 +61,20 @@ export default {
     Search
   },
   computed: mapState({
-    user: state => state.user.user
+    user: (state) => state.user.user
   }),
   methods: {
     signOut() {
-      this.$store.dispatch('user/signOut')
+      this.$store
+        .dispatch('user/signOut')
+        .then(() => {
+          this.$router.replace('/')
+        })
+        .catch(console.log)
     }
   }
 }
 </script>
-
 
 <style lang="scss">
 @import '~assets/scss/components/header';
