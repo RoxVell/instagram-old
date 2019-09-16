505524f9d0487767d7b8bb7f526e91cb700e9349
RoxVell <anton1.a@mail.ru>
2019-09-16 22:19:38 +0300

    Add IconSpinner component and update dependencies

---
+++ components/Icons/IconSpinner.vue
@@ -0,0 +1,36 @@
+<template>
+  <circle
+    cy="50"
+    fill="none"
+    :stroke="color"
+    :stroke-width="strokeWidth"
+    r="35"
+    stroke-dasharray="165 57"
+  >
+    <animateTransform
+      attributeName="transform"
+      type="rotate"
+      calcMode="linear"
+      values="0 50 50;360 50 50"
+      keyTimes="0;1"
+      dur="1s"
+      begin="0s"
+      repeatCount="indefinite"
+    />
+  </circle>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: '#28292f'
+    },
+    strokeWidth: {
+      type: Number,
+      default: 10
+    }
+  }
+}
+</script>
--- components/Spinner.vue
+++ components/Spinner.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="spinner-component">
-    <span class="spinner-icon"></span>
+    <IconBase width="45" height="45" viewbox="0 0 100 100">
+      <IconSpinner />
+    </IconBase>
+    <!-- <span class="spinner-icon"></span> -->
     <span class="spinner-text">Загрузка...</span>
   </div>
 </template>
+
+<script>
+import IconBase from '~/components/Icons/IconBase'
+import IconSpinner from '~/components/Icons/IconSpinner'
+
+export default {
+  components: {
+    IconBase,
+    IconSpinner
+  }
+}
+</script>
 
 <style lang="scss">
 @import '~assets/scss/components/spinner.scss';
