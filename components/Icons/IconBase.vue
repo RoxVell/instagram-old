d8925717c30343bfe001f683faa38bec5b4975e1
RoxVell <anton1.a@mail.ru>
2019-09-16 21:08:07 +0300

    Add Icon Components

---
+++ components/Icons/IconBase.vue
@@ -0,0 +1,48 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    :viewBox="viewbox"
+    :aria-labelledby="name"
+    role="presentation"
+  >
+    <title :id="name" lang="en">{{ name }} icon</title>
+    <g :fill="color">
+      <slot />
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'box'
+    },
+    width: {
+      type: [Number, String],
+      default: 18
+    },
+    height: {
+      type: [Number, String],
+      default: 18
+    },
+    color: {
+      type: String
+    },
+    viewbox: {
+      type: String,
+      default: '0 0 18 18'
+    }
+  }
+}
+</script>
+
+<style scoped>
+svg {
+  display: inline-block;
+  vertical-align: baseline;
+}
+</style>
---
+++ components/Icons/IconLike.vue
@@ -0,0 +1,9 @@
+<template>
+  <path
+    d="M35,8 c-4.176,0-7.851,2.136-10,5.373C22.851,10.136,19.176,8,15,8C8.373,8,3,13.373,3,20c0,14,16,21,22,26c6-5,22-12,22-26 C47,13.373,41.627,8,35,8z"
+    stroke="none"
+    stroke-linecap="round"
+    stroke-miterlimit="10"
+    stroke-width="2"
+  />
+</template>
