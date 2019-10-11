1d8a1d2bc0b8df8975ce03f285f630b9a0925cdd
RoxVell <anton1.a@mail.ru>
2019-10-11 12:12:38 +0300

    Add comment components (Comment & CommentOwner)

---
+++ components/Comment.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="comment-component" :class="{'no-likes': comment.likes === 0}">
+    <div class="comment">
+      <UserAvatar class="comment-avatar" :size="25" :username="comment.username" />
+      <div class="comment-body">
+        <div class="comment-username">
+          <nuxt-link :to="comment.username">{{ comment.username }}</nuxt-link>
+          <div @click="deleteComment">
+            <IconBase
+              class="comment-delete"
+              v-if="comment.isMine"
+              name="delete"
+              width="10"
+              height="10"
+              viewbox="0 0 400 400"
+            >
+              <IconClose />
+            </IconBase>
+          </div>
+        </div>
+
+        <p class="comment-text">{{ comment.text }}</p>
+        <div class="comment-action">
+          <div>
+            <span class="comment-date">{{ comment.timeAgo }}</span>
+            <span
+              v-if="$store.getters['account/isAuth']"
+              class="comment-action__answer"
+              @click="replyToComment"
+            >Ответить</span>
+          </div>
+          <div
+            v-if="$store.getters['account/isAuth']"
+            class="comment-like__section"
+            @click="toggleLike"
+            :class="{ 'comment-liked': comment.isLiked }"
+          >
+            <IconBase name="like" width="12" height="12" viewbox="0 0 50 50">
+              <IconLike />
+            </IconBase>
+            <span class="comment-likes">{{ comment.likes }}</span>
+          </div>
+        </div>
+
+        <div v-if="comment.repliesCount" class="comment-subcomments">
+          <button
+            class="comment-subcomments__button"
+            @click="toggleReplies"
+          >{{ comment.isRepliesLoaded ? '― Скрыть ответы' : `― Посмотреть ответы (${comment.repliesCount})` }}</button>
+          <div>
+            <div v-if="showReplies">
+              <Comment v-for="comment in comment.replies" :comment="comment" :key="comment.id" />
+            </div>
+          </div>
+        </div>
+
+        <div class="comment-reply" v-if="isReplying">
+          <textarea
+            class="comment-reply__input"
+            @input="resizeComment"
+            @keypress="addReply"
+            rows="1"
+            type="text"
+            v-model="replyText"
+            placeholder="Написать комментарий"
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconBase from '~/components/Icons/IconBase'
+import IconLike from '~/components/Icons/IconLike'
+import IconClose from '~/components/Icons/IconClose'
+import UserAvatar from '~/components/UserAvatar'
+
+export default {
+  name: 'Comment',
+  props: {
+    comment: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    IconBase,
+    IconLike,
+    IconClose,
+    UserAvatar
+  },
+  data() {
+    return {
+      isReplying: false,
+      showReplies: true,
+      replyText: '',
+      replies: [],
+      repliesPaginate: null
+    }
+  },
+  watch: {
+    comment(newValue) {
+      console.log('Коммент изменился')
+      console.log(newValue)
+    }
+  },
+  methods: {
+    toggleReplies() {
+      if (this.comment.isRepliesLoaded) {
+        this.showReplies = false
+      } else {
+        this.showReplies = true
+        this.loadReplies()
+      }
+    },
+    async addReply(event) {
+      if (event.keyCode === 13 && event.shiftKey && this.replyText) {
+        event.preventDefault()
+
+        const { data: comment } = await this.$store.dispatch('comment/addComment', {
+          postId: this.postId,
+          commentId: this.comment.id,
+          text: this.replyText,
+          isReply: true
+        })
+
+      }
+    },
+    loadReplies() {
+      if (!this.comment.isRepliesLoading) this.$emit('loadReplies', this.comment.id)
+    },
+    resizeComment(event) {
+      const textarea = event.target
+      textarea.style.height = 'auto'
+      textarea.style.height = textarea.scrollHeight + 2 + 'px'
+    },
+    toggleLike() {
+      this.comment.isLiked ? this.unlikeComment() : this.likeComment()
+    },
+    async likeComment() {
+      try {
+        // FIXME: likeComment function should take comment id and post id
+        await this.$store.dispatch('comment/likeComment', this.comment)
+        this.$emit('likeComment', this.comment)
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async unlikeComment() {
+      try {
+        // FIXME: likeComment function should take comment id and post id
+        await this.$store.dispatch('comment/unlikeComment', this.comment)
+        this.$emit('unlikeComment', this.comment)
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async deleteComment() {
+      try {
+        await this.$store.dispatch('comment/deleteComment', {
+          commentId: this.comment.id,
+          postId: this.postId
+        })
+        this.$emit('deleteComment', this.comment)
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    replyToComment() {
+      this.isReplying = true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~/assets/scss/components/Comment.scss';
+</style>
---
+++ components/CommentOwner.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="comment-component comment-component-owner">
+    <div class="user-info">
+      <div class="user-avatar">
+        <img :src="postOwner.profile_picture" :alt="postOwner.username" />
+      </div>
+      <div class="user-comment">
+        <p class="comment-username">{{ postOwner.username }}</p>
+        <p class="comment-date">{{ post.timeAgo }}</p>
+      </div>
+    </div>
+    <p
+      class="comment-text"
+      v-if="post.description"
+      v-html="post.description.replace(/\s\s/gm, '<br><br>')"
+    ></p>
+  </div>
+</template>
+
+<script>
+import { computeTimeAgo } from '~/utils/index'
+
+export default {
+  props: {
+    postOwner: {
+      type: Object,
+      required: true
+    },
+    post: {
+      type: Object,
+      required: true
+    },
+    date: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/components/Comment.scss';
+</style>
