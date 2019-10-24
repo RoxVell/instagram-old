b8be6448959c02c3e58ed585fecb55d3e3bfaed4
RoxVell <anton1.a@mail.ru>
2019-10-24 22:39:41 +0300

    Adding following system

--- components/UserProfile/Other.vue
+++ components/UserProfile/Other.vue
@@ -2,15 +2,19 @@
   <UserProfile :user="user">
     <template slot="username-section">
       <template v-if="isAuth">
-        <button class="btn btn-edit" @click="follow">Подписаться</button>
+        <Button
+          :loading="isFollowingLoading"
+          :class="{ 'btn_blue': isFollowing }"
+          @click="toggleFollow"
+        >{{ isFollowing ? 'Вы подписаны' : 'Подписаться' }}</Button>
       </template>
-      <template v-else></template>
     </template>
   </UserProfile>
 </template>
 
 <script>
 import UserProfile from '~/components/UserProfile/Default'
+import Button from '~/components/Button'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -21,20 +25,66 @@ export default {
     }
   },
   components: {
-    UserProfile
+    UserProfile,
+    Button
   },
   data() {
     return {
-      isFollowing: null
+      isFollowing: false,
+      isFollowingLoading: false
     }
   },
   computed: {
     ...mapGetters({ isAuth: 'account/isAuth' })
   },
   methods: {
-    follow() {
-      console.log(`Follow that user`, this.user.username)
+    toggleFollow() {
+      this.isFollowing ? this.unfollow() : this.follow()
+    },
+    async follow() {
+      this.isFollowingLoading = true
+
+      try {
+        await this.$store.dispatch('follow/followUser', this.user.username)
+        this.isFollowing = true
+        this.user.followers += 1
+      } catch (error) {
+        console.error(error)
+      }
+
+      this.isFollowingLoading = false
+    },
+    async unfollow() {
+      this.isFollowingLoading = true
+
+      try {
+        await this.$store.dispatch('follow/unfollowUser', this.user.username)
+        this.isFollowing = false
+        this.user.followers -= 1
+      } catch (error) {
+        console.error(error)
+      }
+
+      this.isFollowingLoading = false
+    },
+    async checkIsFollowing() {
+      this.isFollowingLoading = true
+
+      try {
+        this.isFollowing = await this.$store.dispatch('follow/isFollowing', this.user.username)
+      } catch (error) {
+        console.error(error)
+      }
+
+      this.isFollowingLoading = false
     }
+  },
+  created() {
+    this.checkIsFollowing()
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/components/UserProfile/Other.scss';
+</style>
---
+++ store/follow.js
@@ -0,0 +1,41 @@
+import { functions, firestore } from '~/firebase/init'
+import { QueryPaginate } from '../utils/firebaseUtils.ts'
+
+export const state = () => ({})
+
+export const mutations = {}
+
+export const actions = {
+  followUser({ commit }, followedUsername) {
+    const followUserAction = functions.httpsCallable('followUser')
+
+    return followUserAction({ followedUsername }).then(() => {
+      commit('account/UPDATE_FOLLOWING_COUNT', 1, { root: true })
+    })
+  },
+  unfollowUser({ commit }, followedUsername) {
+    const unfollowUserAction = functions.httpsCallable('unfollowUser')
+
+    return unfollowUserAction({ followedUsername }).then(() => {
+      commit('account/UPDATE_FOLLOWING_COUNT', -1, { root: true })
+    })
+  },
+  isFollowing({ rootState }, followedUsername) {
+    const followerUsername = rootState.account.user.username
+
+    return firestore
+      .doc(`followers/${followedUsername}/userFollowing/${followerUsername}`)
+      .get()
+      .then((snapshot) => snapshot.exists)
+  },
+  getFollowersByUsername(_, username) {
+    const query = firestore
+      .collection('followers')
+      .doc(username)
+      .collection('userFollowing')
+
+    return QueryPaginate(query)
+  }
+}
+
+export const strict = false
