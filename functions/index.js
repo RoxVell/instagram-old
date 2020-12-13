44543923e815cfddec9244c6ac8140db49ca03e4
Anton <anton1.a@mail.com>
2020-12-13 15:04:28 +0300

    [firebase functions] add getFavoritesPostsByUsername function

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,12 @@ const fs = require('fs')
 
 admin.initializeApp()
 
-// import and export of all module functions
+// import and export all module functions
 const exportDir = `${__dirname}/modules`
+const except = ['imageCompress.js']
 
 fs.readdirSync(exportDir).forEach((file) => {
+  if (except.includes(file)) return;
   const module = require(`${exportDir}/${file}`)(admin)
 
   for (let functionName in module) {
--- functions/modules/post.js
+++ functions/modules/post.js
@@ -1,8 +1,31 @@
-const functions = require('firebase-functions')
+const functions = require('firebase-functions');
 
 module.exports = function(admin) {
   const increment = admin.firestore.FieldValue.increment(1)
   const decrement = admin.firestore.FieldValue.increment(-1)
+
+  const getFavoritesPostsByUsername = functions.https.onCall(async (data, context) => {
+    const { username } = data
+
+    const favoritePostsIds = await admin.firestore()
+      .collection('favorites_posts')
+      .doc(username)
+      .collection('favorites')
+      .get()
+
+    const favoritePosts = [];
+
+    for (const postDocument of favoritePostsIds.docs) {
+      const post = await admin.firestore().doc(`posts/${postDocument.id}`).get()
+
+      favoritePosts.push({
+        id: postDocument.id,
+        ...post.data()
+      })
+    }
+
+    return Promise.resolve(favoritePosts)
+  });
 
   const onPostLiked = functions.firestore
     .document('post_likes/{postId}/likes/{username}')
@@ -24,6 +47,7 @@ module.exports = function(admin) {
 
   return {
     onPostLiked,
-    onPostUnliked
+    onPostUnliked,
+    getFavoritesPostsByUsername
   }
 }
